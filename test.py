af0a7df1f5df68d54e16368208e70cdc929102d7
Your Name <siripong.trai@gmail.com>
2020-11-03 13:18:09 +0700

    test

--- test.py
+++ test.py
@@ -1,2 +1,21 @@
+import time
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+
 def test():
     print('Test App Service')
+
+def test_2():
+    t_end = time.time() + 60 * 15
+    while time.time() < t_end:
+
+def test_3():
+    X, y = make_classification(n_samples=1000, n_features=4,
+                                n_informative=2, n_redundant=0,
+                                random_state=0, shuffle=False)
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(X, y)
+    print(clf.predict([[0, 0, 0, 0]]))
+
+if __name__ == "__main__":
+    test_2()
